de840e517b817632c4bf429bc910a7a1c4925023
@benjamin lucky <bamidelebenjamin5@gmail.com>
2025-04-05 12:56:24 +0100

    Allowed Origins Updated

--- server/server.js
+++ server/server.js
@@ -18,14 +18,33 @@ mongoose
     console.log(err.message);
   });
 
-const port = process.env.PORT || 5000;
-const allowedOrigins = [process.env.CLIENT_URL];
+// Define allowed origins
+const allowedOrigins = [
+  "http://localhost:3000", // Dev
+  "https://estatesyndicates.netlify.app", // Test
+  "https://estatesindicates.com", // Production
+];
 
-// Middleware
-app.use(cors());
+// CORS middleware
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Enable this if you're using cookies/auth headers
+  })
+);
+
+// Body parser
 app.use(express.json());
 
 // Routes
-app.use("/investor/", investorRoutes);
+app.use("/investor", investorRoutes);
 
+// Server listen
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
